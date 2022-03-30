c98de812e93e41e650dbbe3724618d6bfdb6309d
Locke Tough <locktoug138@ed.amdsb.ca>
2022-03-30 14:39:48 -0400

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.choice1 = new System.Windows.Forms.Button();
+            this.choice2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // choice1
+            // 
+            this.choice1.BackColor = System.Drawing.Color.Black;
+            this.choice1.Font = new System.Drawing.Font("Viner Hand ITC", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choice1.ForeColor = System.Drawing.Color.Transparent;
+            this.choice1.Location = new System.Drawing.Point(12, 321);
+            this.choice1.Name = "choice1";
+            this.choice1.Size = new System.Drawing.Size(280, 58);
+            this.choice1.TabIndex = 1;
+            this.choice1.Text = "button1";
+            this.choice1.UseVisualStyleBackColor = false;
+            this.choice1.Click += new System.EventHandler(this.choice1_Click);
+            // 
+            // choice2
+            // 
+            this.choice2.BackColor = System.Drawing.Color.Black;
+            this.choice2.Font = new System.Drawing.Font("Viner Hand ITC", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choice2.ForeColor = System.Drawing.Color.Transparent;
+            this.choice2.Location = new System.Drawing.Point(314, 321);
+            this.choice2.Name = "choice2";
+            this.choice2.Size = new System.Drawing.Size(280, 58);
+            this.choice2.TabIndex = 2;
+            this.choice2.Text = "button2";
+            this.choice2.UseVisualStyleBackColor = false;
+            this.choice2.Click += new System.EventHandler(this.choice2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 128);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AdventureGame.Properties.Resources.alley;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(612, 429);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(606, 391);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.choice2);
+            this.Controls.Add(this.choice1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button choice1;
+        private System.Windows.Forms.Button choice2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        SoundPlayer jumpscareplayer = new SoundPlayer(Properties.Resources.Jumpscare_Sound_Effect);
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "You are walking down an alley way when you notice a man following you. What will you do?";
+            choice1.Text = "Run";
+            choice2.Text = "Confront him";
+            pictureBox1.Image = Properties.Resources.alley;
+        }
+
+        private void choice1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 3)
+            {
+                page = 1;            
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 5)
+            {
+                page = 1;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            { 
+                page = 1; 
+            }
+            else if  (page == 7)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+
+            
+
+            switch (page)
+            {
+                case 1:
+                    label1.Text = "You are walking down an alley way when you notice a man following you. What will you do?";
+                    choice1.Text = "Run";
+                    choice2.Text = "Confront him";
+                    pictureBox1.Image = Properties.Resources.alley;
+                    break;
+
+                case 2:
+                    label1.Text = "He is right behind you. You can go through the door or go left. What will you choose?";
+                    choice1.Text = "Door";
+                    choice2.Text = "Left";
+                    pictureBox1.Image = Properties.Resources.door;
+                    break;
+                    case 4:
+                    label1.Text = "You enter. You can go upstairs or the basement.";
+                    choice1.Text = "Upstairs";
+                    choice2.Text = "Basement";
+                    pictureBox1.Image = Properties.Resources.inside;
+                    break;
+
+                case 6:
+                    label1.Text = "As you're running up the stairs you slip. You die from your injuries. Play Again?";
+                    choice1.Text = "Yes";
+                    choice2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.stair_ending;
+                    break;
+
+                case 8:
+                    label1.Text = "You run for the exit and slam the door on him. You get away. Play Again?";
+                    choice1.Text = "Yes";
+                    choice2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.good_ending;
+                        break;
+                case 10:
+                    label1.Text = "";
+                    choice1.Text = "";
+                    choice2.Text = "";
+                    pictureBox1.Image = Properties.Resources.jumpscare;
+                    jumpscareplayer.Play();
+                    break;
+
+
+
+                   
+                      
+                    
+
+            }
+        }
+
+        private void choice2_Click (object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page ==3)
+            {
+                Application.Exit();
+            }
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                Application.Exit();
+            }
+            else if (page == 6)
+            {
+                Application.Exit();
+            }
+            else if (page == 4)
+            {
+                page = 7;
+            }
+            else if (page == 8)
+            {
+                Application.Exit();
+            }
+            else if (page == 7)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+
+
+            switch (page)
+
+            {
+                case 3:
+                    label1.Text = "You are kidnapped and never seen again. Play again?";
+                    choice1.Text = "Yes";
+                    choice2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.kidnapped_ending;
+                    break;
+
+                case 5:
+                    label1.Text = "You run as you fast as you can but it's not enough. You are caught and taken away. Play Again?";
+                        choice1.Text = "Yes";
+                    choice2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.kidnapped_ending;
+                    break;
+
+                case 7:
+                    label1.Text = "It is very dark, the only light you see is from the exit.";
+                    choice1.Text = "Run for exit";
+                    choice2.Text = "Stay put";
+                    pictureBox1.Image = Properties.Resources.basement;
+                    break;
+
+                case 9:
+                    label1.Text = "...";
+                    choice1.Text = ":)";
+                    choice2.Text = ":)";
+                    pictureBox1.Image = null;                   
+                    break;
+
+                case 10:
+                    label1.Text = "";
+                    choice1.Text = "";
+                    choice2.Text = "";
+                    pictureBox1.Image = Properties.Resources.jumpscare;
+                    jumpscareplayer.Play();                   
+                    break;
+
+
+
+
+
+            }
+
+
+        }
+
+       
+        }
+
+
+        }
+
+        
+    
+
